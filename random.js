30677a0b5a5b18d1eab425441e4e2e26184b6dfc
fahad1226 <54905485+AjAsif49@users.noreply.github.com>
2022-02-20 22:08:46 +0600

    frst

---
+++ random.js
@@ -0,0 +1,16 @@
+var num = 2.1893763;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+
+for(var i = 0; i < 10; i++){
+    var result4 = Math.random(num) * 10;
+    var result5 = Math.round(result4)
+    console.log(result5);
+
+}
