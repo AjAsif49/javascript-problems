09a689adc8e874d75c0ded9de39f156850a4400c
fahad1226 <54905485+AjAsif49@users.noreply.github.com>
2022-02-26 23:48:27 +0600

    arraySum

--- arraySum.js
+++ arraySum.js
@@ -9,10 +9,20 @@ function arraySum(marks){
     return sum;    
 }
 
-var marks = [52, 21, 88, 56, 98 , 55, 65, 70];
+var marks = [52, 21, 88, 56, 98 , 55, 65, 70, 76, 89, 09, 99];
 var result = arraySum(marks);
 console.log(result);
 
 var total = arraySum([10, 54, 78, 65, 78, 55]);
 console.log(total);
 
+
+
+var array = [1, 2, 3, 4, 5];
+    
+    // Getting sum of numbers
+    var sum = array.reduce(function(a, b){
+        return a + b;
+    }, 0);
+    
+    console.log(sum);
